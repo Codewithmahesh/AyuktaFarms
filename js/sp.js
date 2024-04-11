5fb2885ea80164ade240d5cba4ff45724c73670f
Codewithmahesh <girimahesh1514@gmail.com>
2024-04-11 09:56:11 +0530

    single product

--- js/sp.js
+++ js/sp.js
@@ -1,3 +1,27 @@
+const imgs = document.querySelectorAll(".img-select a");
+const imgBtns = [...imgs];
+let imgId = 1;
+
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener("click", (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage() {
+    const displayWidth = document.querySelector(
+        ".img-showcase img:first-child"
+    ).clientWidth;
+
+    document.querySelector(".img-showcase").style.transform = `translateX(${-(imgId - 1) * displayWidth
+        }px)`;
+}
+
+window.addEventListener("resize", slideImage);
+
+
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
@@ -5,5 +29,3 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-
-
